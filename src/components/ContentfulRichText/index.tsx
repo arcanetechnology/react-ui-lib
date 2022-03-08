ac27dfd8d7eafa3a7ed75b4d61b8083c2216ac47
Neverlastinger <shadow_87@mail.bg>
2022-03-08 08:58:01 +0100

    Fix: no new lines in Contentful Rich Text

--- src/components/ContentfulRichText/index.tsx
+++ src/components/ContentfulRichText/index.tsx
@@ -28,6 +28,9 @@ const renderOptions = (links: any) => {
         );
       },
     },
+    renderText: (text: any) => (
+      text.split('\n').flatMap((text: any, i: number) => [i > 0 && <br />, text])
+    )
   };
 };
 
